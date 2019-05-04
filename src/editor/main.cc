682644ffd0c2a8c145f93c223d63fcf94df2a360
cjdouglas <chris.douglas.210@gmail.com>
2019-05-04 13:38:11 -0500

    Added installation for nanogui

--- src/editor/main.cc
+++ src/editor/main.cc
@@ -1,10 +1,29 @@
 #include "rendering/renderer.cc"
+#include <nanogui/nanogui.h>
+
+using namespace nanogui;
 
 int main(void) {
-  Renderer renderer;
-  if (!renderer.InitializePrograms()) {
-    std::cout << "Renderer failed to initalize programs" << std::endl;
-    return -1;
-  }
-  renderer.DrawFrame();
+  nanogui::init();
+  Screen *screen = new Screen(Vector2i(1500, 700), "NanoGUI Test");
+  FormHelper *gui = new FormHelper(screen);
+  ref<Window> window =
+      gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
+  gui->addGroup("Other widgets");
+  gui->addButton("A button",
+                 []() { std::cout << "Button pressed." << std::endl; });
+
+  screen->setVisible(true);
+  screen->performLayout();
+  window->center();
+
+  nanogui::mainloop();
+  /*
+Renderer renderer;
+if (!renderer.InitializePrograms()) {
+std::cout << "Renderer failed to initalize programs" << std::endl;
+return -1;
+}
+renderer.DrawFrame();
+  */
 }
